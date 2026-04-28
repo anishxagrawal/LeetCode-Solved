cbe71369351f0172b22c17886bc9e479b1852b03
anish agrawal <166867261+anishxagrawal@users.noreply.github.com>
2026-04-28 19:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.69%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -12,13 +12,13 @@ public:
             if (target == nums[mid]) return mid;
 
             else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target <= nums[mid])
+                if (nums[left] <= target && target < nums[mid])
                     right = mid - 1;
                 else left = mid + 1;
             }
 
             else if (nums[mid] <= nums[right]){
-                if (nums[mid] <= target && target <= nums[right])
+                if (nums[mid] < target && target <= nums[right])
                     left = mid + 1;
                 else right = mid - 1;
             }
